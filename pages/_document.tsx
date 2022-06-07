d7cdb794c49778d6bec6f0ce0286d4e587fa0e3a
Jilver Pacheco <jilverrivera1@gmail.com>
2022-06-07 15:01:58 -0500

    add font weight

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,7 +26,7 @@ class MyDocument extends Document {
             crossOrigin="true"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Cairo&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
         </Head>
