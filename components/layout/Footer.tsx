4f988477177856371b9fb540e2aa5e6eda9b823a
Jilver Pacheco <jilverrivera1@gmail.com>
2022-06-06 22:02:22 -0500

    add footer component

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,5 +1,36 @@
+import { networks } from "../../helpers/networks";
+
 const Footer = () => {
-  return <div className="h-96 flex items-center justify-center">Footer</div>;
+  return (
+    <div className="h-96 flex flex-col items-center justify-between">
+      <div className="flex flex-col items-center justify-center mt-16">
+        <h2 className="text-4xl font-semibold mb-3">Let&apos;s Connect!</h2>
+        <p className="text-base font-normal mb-5">
+          Feel free to send me a message on my social networks
+        </p>
+        <div className="flex items-center justify-center">
+          {networks.map((item, i) => (
+            <a
+              href={item.url}
+              key={i}
+              className={`mx-2 w-10 h-10 flex items-center justify-center rounded-full`}
+            >
+              <span className="text-2xl">{item.icon}</span>
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className="w-full flex flex-col justify-center items-center mb-3">
+        <span className="text-slate-400 text-sm mb-2">
+          Jilver Pacheco Rivera • @jilverprivera • All rights reserved © 2020 -{" "}
+          {new Date().getFullYear()}
+        </span>
+        <span className="text-slate-400 text-sm">
+          From Colombia ❤ to the world
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
---
+++ helpers/networks.tsx
@@ -0,0 +1,33 @@
+import { BsGithub, BsLinkedin, BsInstagram, BsTwitter } from "react-icons/bs";
+
+export const networks = [
+  {
+    url: "https://github.com/jilverprivera",
+    icon: <BsGithub />,
+    name: "Github",
+    bgColor: "#F8F8F8",
+    color: "#000000",
+  },
+  {
+    url: "https://www.linkedin.com/in/jilverprivera/",
+    icon: <BsLinkedin />,
+    name: "Linkedin",
+    bgColor: "#0A66C2",
+    color: "#FFFFFF",
+  },
+  {
+    url: "https://www.instagram.com/jilverprivera/",
+    icon: <BsInstagram />,
+    name: "Instagram",
+    bgColor:
+      "radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%)",
+    color: "#FFFFFF",
+  },
+  {
+    url: "https://twitter.com/Jilverprivera",
+    icon: <BsTwitter />,
+    name: "Twitter",
+    bgColor: "#1A8CD8",
+    color: "#FFFFFF",
+  },
+];
