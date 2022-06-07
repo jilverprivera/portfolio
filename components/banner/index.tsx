7eeb8f36db068a66db8c3a4b28f28c188616527c
Jilver Pacheco <jilverrivera1@gmail.com>
2022-06-06 23:39:03 -0500

    add typewritter effect

--- components/banner/index.tsx
+++ components/banner/index.tsx
@@ -1,31 +1,56 @@
 import { Link as NavLink } from "react-scroll";
+import Typewriter from "typewriter-effect";
 import { BsArrowRight } from "react-icons/bs";
+import Image from "next/image";
 const Banner = () => {
   return (
-    <div className="w-full min-h-screen flex flex-col items-start justify-center max-w-7xl mx-auto">
-      <h1 className="text-xl mb-3">I&apos;m Jilver Pacheco</h1>
-      <h2 className="text-7xl font-bold mb-3 theme-primary">
-        Software Developer
-      </h2>
-      <div className="w-2/6">
-        <p className="text-base font-normal">
-          Welcome to my little space on the WWW, where I show my projects and
-          talk about what I like.
-        </p>
-      </div>
-      <NavLink
-        to={"about"}
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={1000}
-        className="font-normal mt-7 flex items-center justify-center cursor-pointer"
+    <div className="min-h-screen flex items-center justify-between max-w-7xl mx-auto">
+      <div
+        className="
+      flex flex-col items-start justify-center "
       >
-        <span className="mr-2">Read more</span>
-        <span className="w-10 h-10 flex items-center justify-center bg-black text-white rounded-full">
-          <BsArrowRight />
-        </span>
-      </NavLink>
+        <h1 className="text-xl mb-3">I&apos;m Jilver Pacheco</h1>
+        <h2 className="text-7xl font-bold mb-3 dark:text-yellow-500 text-violet-800">
+          <Typewriter
+            options={{
+              strings: [
+                "Software Developer",
+                "Electronic Engineer",
+                "Robotics Enthusiast",
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </h2>
+        <div className="w-5/6">
+          <p className="text-base font-normal">
+            Welcome to my little space on the WWW, where I show my projects and
+            talk about what I like.
+          </p>
+        </div>
+        <NavLink
+          to={"about"}
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={1000}
+          className="font-normal mt-10 flex items-center justify-center cursor-pointer"
+        >
+          <span className="mr-2">Read more</span>
+          <span className="w-10 h-10 flex items-center justify-center bg-black dark:bg-yellow-500 text-white dark:text-black rounded-full">
+            <BsArrowRight />
+          </span>
+        </NavLink>
+      </div>
+      <div className="overflow-hidden rounded-full mr-3">
+        <Image
+          src={`/image.webp`}
+          alt="Jilver Pacheco"
+          width={412}
+          height={512}
+        />
+      </div>
     </div>
   );
 };
