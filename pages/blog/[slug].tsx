565c41c463fd5fa90ba130c12644bf6beece77f2
Jilver Pacheco <jilverrivera1@gmail.com>
2022-06-12 10:02:08 -0500

    fixing page

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -6,7 +6,7 @@ import { getFileBySlug, getFiles } from "../../lib/mdx";
 const Post = ({ source, frontmatter }: any) => {
   return (
     <Layout
-      type="posts"
+      type="post"
       metadata={{
         title: frontmatter.title,
         description: frontmatter.description,
@@ -14,7 +14,17 @@ const Post = ({ source, frontmatter }: any) => {
         date: frontmatter.date,
       }}
     >
-      <div className="w-full principal flex items-center justify-center">
+      <div className="border-2 border-black principal flex flex-col items-center justify-center">
+        <span>{frontmatter.date}</span>
+        <h1 className="text-5xl font-bold">{frontmatter.title}</h1>
+        <div>
+          {frontmatter.tags.map((item: string, i: number) => (
+            <span key={i} className="text-slate-500 uppercase text-sm mx-2">
+              {item}
+            </span>
+          ))}
+        </div>
+
         <h1 className="text-5xl font-bold">Currently working on it</h1>
       </div>
     </Layout>
