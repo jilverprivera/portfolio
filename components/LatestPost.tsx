070f8cb713e829c8bc9188fcb63362e47d72ab68
Jilver Pacheco <jilverrivera1@gmail.com>
2022-06-12 09:58:14 -0500

    redesign latest post component

--- components/LatestPost.tsx
+++ components/LatestPost.tsx
@@ -1,59 +1,75 @@
 import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+import { BsArrowRight } from "react-icons/bs";
 import { motion } from "framer-motion";
+
 import { Blog, Post } from "../interfaces/posts";
 
-const LatestPost = ({ posts }: any) => {
+const LatestPost = ({ posts }: Blog) => {
+  const ref = useRef<HTMLDivElement | any>();
+  const [currentWidth, setCurrentWidth] = useState<number>(0);
+  useEffect(() => {
+    setCurrentWidth(ref.current?.scrollWidth - ref.current?.offsetWidth);
+  }, []);
+
   return (
     <div
       id="latest-posts"
-      className="relative max-w-6xl min-h-screen flex flex-col items-start justify-start mx-auto"
+      className="bg-slate-100 dark:bg-zinc-900 overflow-hidden"
     >
-      <div className=" w-full pt-16 pb-8 flex items-center justify-between mb-8">
-        <div>
-          <h2 className="text-5xl font-bold mb-3">Latest Posts</h2>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <span className="uppercase text-sm text-gray-500 dark:text-gray-300 mb-2">
-            Want to view all my posts?
-          </span>
-          <Link href="/blog">
-            <span className="text-base cursor-pointer">
-              jilverpacheco.vercel.app
-              <span className="text-base text-violet-800 dark:text-yellow-500">
-                /blog
-              </span>
+      <div className="max-w-7xl mx-auto latest-post flex flex-col items-center justify-between mb-16">
+        <div className="w-full mx-auto pt-10 flex items-center justify-between mb-5">
+          <h2 className="text-4xl font-semibold mb-3">Latest Posts</h2>
+          <div className="flex flex-col items-center justify-center">
+            <span className="uppercase text-sm text-gray-500 dark:text-gray-300 mb-2">
+              View all posts
             </span>
-          </Link>
+            <Link href="/blog">
+              <a>
+                <span className="text-base cursor-pointer">
+                  jilverpacheco.vercel.app
+                  <span className="text-base text-violet-800 dark:text-yellow-500">
+                    /blog
+                  </span>
+                </span>
+              </a>
+            </Link>
+          </div>
         </div>
-      </div>
-      {posts.map((item: Post, index: number) => (
         <motion.div
-          className="mb-5"
-          key={item.slug}
-          initial={{ opacity: 0, y: -10 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: 10 }}
-          transition={{ delay: 0.2 * index }}
+          drag="x"
+          dragConstraints={{ right: 0, left: -currentWidth }}
+          ref={ref}
+          className="w-full flex flew-row items-center justify-start"
         >
-          <Link href={`/blog/${item.slug}`}>
-            <a className="cursor-pointer">
-              <h2 className="text-sm font-normal">{item.date}</h2>
-              <h2 className="text-xl font-semibold">{item.title}</h2>
-              <div>
-                {item.tags.map((tag, i: number) => (
-                  <span
-                    key={i}
-                    className="mr-3 uppercase text-sm font-normal text-violet-800 dark:text-yellow-500"
-                  >
-                    {tag}
+          {posts.map((post: Post, i: number) => (
+            <div
+              key={i}
+              className="post-card h-96 rounded-xl mr-8 p-6  bg-white dark:bg-zinc-800"
+            >
+              <span className="text-sm text-gray-400">{post.date}</span>
+              <h2 className="text-xl font-bold leading-7 mt-2">{post.title}</h2>
+              <div className="my-2">
+                {post.tags.map((item, i) => (
+                  <span key={i} className="text-sm text-gray-400 mr-2">
+                    {item}
                   </span>
                 ))}
               </div>
-            </a>
-          </Link>
-          <p className="text-base font-medium">{item.description}</p>
+              <p className="leading-7 text-base mb-8">{post.description}</p>
+
+              <Link href={`/blog/${post.slug}`}>
+                <a className="flex items-center justify-start">
+                  <span className="text-md mr-2">Read more</span>
+                  <span className="w-10 h-10 rounded-full flex items-center justify-center bg-black text-white">
+                    <BsArrowRight />
+                  </span>
+                </a>
+              </Link>
+            </div>
+          ))}
         </motion.div>
-      ))}
+      </div>
     </div>
   );
 };
