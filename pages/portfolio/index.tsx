75e8ff86d2301e5cdb04b75867c8e6f3aa8cf2cb
Jilver Pacheco <jilverrivera1@gmail.com>
2022-06-13 13:20:22 -0500

    add link to each project card

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -24,54 +24,51 @@ const Portfolio = ({ projects }: Projects) => {
     >
       <div className="w-full principal grid grid-cols-3">
         {projects.map((project: Project, index: number) => (
-          <div
-            key={project.slug}
-            className="relative overflow-hidden"
-            onMouseEnter={() => setCurrentHover(index)}
-            onMouseLeave={() => setCurrentHover(null)}
-          >
-            <Image
-              src={project.cover_image}
-              alt={project.title}
-              width={512}
-              height={360}
-              layout="responsive"
-              objectFit="cover"
-            />
-            <AnimatePresence exitBeforeEnter>
-              {currentHover === index && (
-                <motion.div
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  transition={{ duration: 0.4 }}
-                  className="absolute top-0 left-0 right-0 p-4 flex flex-col items-center justify-center bg-project-info"
-                >
-                  {/* <p className="text-9xl font-bold text-zinc-900">
-                    0{index + 1}
-                  </p> */}
-                  <h2 className="text-lg text-center text-white">
-                    {project.title}
-                  </h2>
-                  <div>
-                    {project.categories.map((item, i) => (
-                      <span key={i} className="text-sm text-white">
-                        {item}
-                      </span>
-                    ))}
-                  </div>
-                  <Link href={`/portfolio/${project.slug}`}>
-                    <a className="  text-white absolute bottom-10 flex items-center justify-center">
-                      <span className="text-sm mr-2">View project</span>
-                      <span className="text-3xl w-12 h-12 bg-black rounded-full flex items-center justify-center">
-                        <BsArrowRight />
-                      </span>
-                    </a>
-                  </Link>
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </div>
+          <Link href={`/portfolio/${project.slug}`} key={project.slug}>
+            <a
+              className="relative overflow-hidden"
+              onMouseEnter={() => setCurrentHover(index)}
+              onMouseLeave={() => setCurrentHover(null)}
+            >
+              <Image
+                src={project.cover_image}
+                alt={project.title}
+                width={512}
+                height={360}
+                layout="responsive"
+                objectFit="cover"
+              />
+              <AnimatePresence exitBeforeEnter>
+                {currentHover === index && (
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.4 }}
+                    className="absolute top-0 left-0 right-0 p-4 flex flex-col items-center justify-center bg-project-info"
+                  >
+                    <div>
+                      {project.categories.map((item, i) => (
+                        <span key={i} className="text-sm text-white mr-2">
+                          {item}
+                        </span>
+                      ))}
+                    </div>
+                    <h2 className="text-xl font-bold text-center text-white">
+                      {project.title}
+                    </h2>
+                    <div>
+                      {project.technologies.map((item, i) => (
+                        <span key={i} className="text-sm text-white mr-2">
+                          {item}
+                        </span>
+                      ))}
+                    </div>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </a>
+          </Link>
         ))}
       </div>
     </Layout>
