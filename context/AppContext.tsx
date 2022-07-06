9641c6a815c7c216f860f8ff6a86704d72b17a83
Jilver Pacheco <jilverrivera1@gmail.com>
2022-07-06 13:03:06 -0500

    create Context

---
+++ context/AppContext.tsx
@@ -0,0 +1,26 @@
+import { createContext, useState } from "react";
+
+type contextProps = {
+  cursor: string;
+  //   setCursor: (arg: string) => void;
+  cursorEnter: () => void;
+  cursorLeave: () => void;
+};
+
+interface providerProps {
+  children: JSX.Element | JSX.Element[];
+}
+export const AppContext = createContext({} as contextProps);
+
+export const AppProvider = ({ children }: providerProps) => {
+  const [cursor, setCursor] = useState("default");
+
+  const cursorEnter = () => setCursor("text");
+  const cursorLeave = () => setCursor("default");
+
+  return (
+    <AppContext.Provider value={{ cursor, cursorEnter, cursorLeave }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
