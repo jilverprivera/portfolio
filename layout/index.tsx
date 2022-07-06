cef89b25b9b32fd513eb66a7fabf22775e121509
Jilver Pacheco <jilverrivera1@gmail.com>
2022-07-06 11:35:22 -0500

    change layout for page transitions

--- layout/index.tsx
+++ layout/index.tsx
@@ -1,22 +1,44 @@
 import SEO from "../seo";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 import { layoutProps } from "../interfaces/layout";
+import { domAnimation, LazyMotion, m } from "framer-motion";
+import PAGE_TRANSITION from "../utils/pageTransitionVariants";
+import { useWindow } from "../hooks/useWindow";
 
+const layoutMotionProps = {
+  initial: { opacity: 0, y: -20 },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: PAGE_TRANSITION.DURATION,
+      ease: PAGE_TRANSITION.EASE,
+    },
+  },
+  exit: {
+    opacity: 0,
+    y: -20,
+    transition: {
+      duration: PAGE_TRANSITION.DURATION,
+      ease: PAGE_TRANSITION.EASE,
+    },
+  },
+};
 const Layout = ({ children, metadata, type }: layoutProps) => {
   const { title, description, slug, date } = metadata;
+  const { mousePosition } = useWindow();
 
   return (
     <>
       <SEO title={title} description={description} slug={slug} date={date} />
-      <Header />
-      <div
-        className={`mx-auto ${type === "post" ? "max-w-5xl mt-8" : "w-full"}`}
-      >
-        {children}
-      </div>
-      <Footer />
+      <LazyMotion features={domAnimation}>
+        <m.main
+          className={`mx-auto w-full ${type === "post" && "max-w-5xl mt-8"}`}
+          {...layoutMotionProps}
+        >
+          {children}
+        </m.main>
+      </LazyMotion>
     </>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,29 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
+import { AnimatePresence } from "framer-motion";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
+
+import "../styles/globals.css";
+
+const MyApp = ({
+  Component,
+  pageProps: { session, ...pageProps },
+  router,
+}: AppProps) => {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       <SessionProvider session={session}>
-        <Component {...pageProps} />
+        <Header />
+        <AnimatePresence exitBeforeEnter initial={false}>
+          <Component {...pageProps} key={router.asPath} />
+        </AnimatePresence>
+        <Footer />
       </SessionProvider>
     </ThemeProvider>
   );
-}
+};
 
 export default MyApp;
