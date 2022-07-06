bcdd537ad2dea6636944598ff269a6daeabf4721
Jilver Pacheco <jilverrivera1@gmail.com>
2022-07-06 11:37:23 -0500

    custom hooks for window events

---
+++ hooks/useWindow.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+export const useWindow = () => {
+  const [isRendering, setIsRendering] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [windowSize, setWindowSize] = useState({ width: 0, height: 0 });
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsRendering(true);
+
+      const handleMouseMove = (e: any) => {
+        console.log(e);
+      };
+
+      const handleScreenResize = () => {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      };
+
+      window.addEventListener("mousemove", handleMouseMove);
+      window.addEventListener("resize", handleScreenResize);
+
+      return () => {
+        window.addEventListener("mousemove", handleMouseMove);
+        window.removeEventListener("resize", handleScreenResize);
+      };
+    } else {
+      setIsRendering(false);
+    }
+  }, []);
+
+  return { isRendering, windowSize, mousePosition };
+};
+
+// const handleScroll = event => {
+//     console.log('window.scrollY', window.scrollY);
+//   };
+
+//   window.addEventListener('scroll', handleScroll);
+
+//   return () => {
+//     window.removeEventListener('scroll', handleScroll);
+//   };
