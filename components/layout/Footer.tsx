0f2fc78409a0d98a6ab2c5786fa36ae367dd4ec7
Jilver Pacheco <jilverrivera1@gmail.com>
2022-07-06 22:36:01 -0500

    remove networks

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,25 +2,8 @@ import { networks } from "../../helpers/networks";
 
 const Footer = () => {
   return (
-    <div className="h-96 flex flex-col items-center justify-between">
-      <div className="flex flex-col items-center justify-center mt-16">
-        <h2 className="text-4xl font-semibold mb-3">Let&apos;s Connect!</h2>
-        <p className="text-base font-normal mb-5">
-          Feel free to send me a message on my social networks
-        </p>
-        <div className="flex items-center justify-center">
-          {networks.map((item, i) => (
-            <a
-              href={item.url}
-              key={i}
-              className={`mx-2 w-10 h-10 flex items-center justify-center rounded-full`}
-            >
-              <span className="text-2xl">{item.icon}</span>
-            </a>
-          ))}
-        </div>
-      </div>
-      <div className="w-full flex flex-col justify-center items-center mb-3">
+    <div className="flex flex-col items-center justify-between mt-32">
+      <div className="w-full flex flex-col justify-center items-center mb-8">
         <span className="text-slate-700 text-sm mb-2 dark:text-gray-300">
           Jilver Pacheco Rivera • @jilverprivera • All rights reserved © 2020-
           {new Date().getFullYear()}
